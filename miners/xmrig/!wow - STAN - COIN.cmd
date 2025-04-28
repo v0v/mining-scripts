7faf455daa8d943fa2c32f0e48cec3f9e53c3b35
v0v <vbhome@mail.ru>
2025-04-28 10:19:26 +0300

    Remove folder_name from tracking

--- miners/xmrig/!wow - STAN - COIN.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-:start
-    xmrig.exe  -a rx/wow -o stanvps.ddns.net:8120 -u STAN-PROXY.stan69--2660k ^
-        --http-port 37329 --http-no-restricted --http-access-token auth
-goto start
-pause
